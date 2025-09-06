a26dfb4223974cdec7a6e917c635d0f27ed75e95
keshavraj936 <keshavraj936@gmail.com>
2025-09-07 03:07:31 +0530

    7th SEP

---
+++ src/Array_List.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Array_List {
+    public static void main(String[] args) {
+
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(4);
+        list.add(76);
+
+//        System.out.println(list.get(2));
+//        System.out.println(list.size());
+
+//        for(int i = 0; i < list.size() ; i++){
+//            System.out.println(list.get(i));
+//        }
+
+
+//        for(int x : list){
+//            System.out.println(x);
+//        }
+//        System.out.println(list.contains(4));
+//        System.out.println(list.contains(50));
+
+//        list.remove(2);
+//        System.out.println(list.size());
+
+//        list.add(2,50);
+//        for(int x : list){
+//            System.out.println(x);
+//        }
+
+
+        list.set(2,40);
+        System.out.println(list);
+
+
+
+    }
+}
