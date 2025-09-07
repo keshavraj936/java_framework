d7c38f90396b22ba3198b2f5d11e2c9bae0b8b30
keshavraj936 <keshavraj936@gmail.com>
2025-09-08 00:25:32 +0530

    8th SEP

--- src/Array_List.java
+++ src/Array_List.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -6,10 +7,10 @@ public class Array_List {
     public static void main(String[] args) {
 
         //LEARNING FRAMEWORK
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(4);
-        list.add(76);
+        ArrayList<Integer> list1 = new ArrayList<>();
+//        list.add(1);
+//        list.add(4);
+//        list.add(76);
 
 //        System.out.println(list.get(2));
 //        System.out.println(list.size());
@@ -34,9 +35,44 @@ public class Array_List {
 //        }
 
 
-        list.set(2,40);
-        System.out.println(list);
+//        list.set(2,40);
+//        System.out.println(list);
 
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(4);
+        list2.add(2);
+        list2.add(3);
+        list2.add(0,0);
+        System.out.println(list2);
+
+        List<Integer> list3 = List.of(4,5,6,7,8,9);
+
+        list2.addAll(list3);
+        System.out.println(list2);
+
+        Object[] array = list2.toArray();
+        list2.toArray(new Integer[0]);
+
+
+        list2.remove(Integer.valueOf(1));
+        System.out.println(list2);
+
+       list2.sort(null);
+        System.out.println(list2);
+
+
+        List<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Apple");
+        fruits.add("banana");
+        fruits.add("Coconut");
+        fruits.add("Date");
+
+        fruits.remove("Apple");
+
+        System.out.println(fruits);
 
 
 
---
+++ src/Comparator.java
@@ -0,0 +1,5 @@
+public class Comparator {
+    public static void main(String[] args) {
+
+    }
+}
