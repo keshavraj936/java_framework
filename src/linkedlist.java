cffcf0c71c90179dcef12209a6a484f090058095
keshavraj936 <keshavraj936@gmail.com>
2025-09-09 23:34:03 +0530

    9th SEP

--- src/linkedlist.java
+++ src/linkedlist.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class linkedlist {
@@ -8,6 +9,26 @@ public class linkedlist {
         linkedlist.add(1);
         linkedlist.add(2);
         linkedlist.add(3);
+
+        linkedlist.addLast(4);
+        linkedlist.addFirst(0);
+
+        linkedlist.get(2); //O(n)
+        linkedlist.getFirst();
+        linkedlist.getLast();
+
+        System.out.println(linkedlist);
+
+        linkedlist.removeIf(x -> x % 2 == 0);
+        System.out.println(linkedlist);
+
+        LinkedList<String> animals = new LinkedList<>(Arrays.asList("Cat", "Dog", "Elephant"));
+        LinkedList<String> animalsToRemove = new LinkedList<>(Arrays.asList("Dog", "Lion"));
+
+        animals.removeAll(animalsToRemove);
+        System.out.println(animals);
+
+
 
     }
 }
