241df2e2abae4608dadaa9de42e70fc694af6110
keshavraj936 <keshavraj936@gmail.com>
2025-09-09 22:47:22 +0530

    9th SEP

---
+++ src/Making_of_linkedlist.java
@@ -0,0 +1,21 @@
+public class Making_of_linkedlist {
+    public static void main(String[] args) {
+
+        Node node1 = new Node();
+        Node node2 = new Node();
+        node1.value = 1;
+        node1.next =null;
+
+        node2.value = 2;
+        node2.next = null;
+
+    }
+
+}
+
+class Node{
+
+    public int value;
+
+    public Node next;
+}
--- src/linkedlist.java
+++ src/linkedlist.java
@@ -1,5 +1,13 @@
+import java.util.LinkedList;
+
 public class linkedlist {
     public static void main(String[] args) {
+
+
+        LinkedList<Integer> linkedlist = new LinkedList<>();
+        linkedlist.add(1);
+        linkedlist.add(2);
+        linkedlist.add(3);
 
     }
 }
