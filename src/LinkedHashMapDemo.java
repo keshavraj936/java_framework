c25e8ae222c9a0b6454a30836d0baee99605189f
Keshav <keshavraj936@gmail.com>
2025-09-13 23:24:08 +0530

    13th SEP

--- src/LinkedHashMapDemo.java
+++ src/LinkedHashMapDemo.java
@@ -1,9 +1,20 @@
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class LinkedHashMapDemo {
     public static void main(String[] args) {
 
         LinkedHashMap<String, Integer> linkedHashMap = new LinkedHashMap<>();
+
+
+        linkedHashMap.put("Orange", 10);
+        linkedHashMap.put("Apple", 20);
+        linkedHashMap.put("Guava", 15);
+
+        for(Map.Entry<String, Integer> entry : linkedHashMap.entrySet()){
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
 
     }
 }
