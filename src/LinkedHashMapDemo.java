16671c1bf906ac1617845f3e9b9e28e49f3f5e00
Keshav <keshavraj936@gmail.com>
2025-09-13 22:30:43 +0530

    13th SEP

---
+++ src/LinkedHashMapDemo.java
@@ -0,0 +1,9 @@
+import java.util.LinkedHashMap;
+
+public class LinkedHashMapDemo {
+    public static void main(String[] args) {
+
+        LinkedHashMap<String, Integer> linkedHashMap = new LinkedHashMap<>();
+
+    }
+}
