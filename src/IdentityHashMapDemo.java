1555c19419d9834b283cf20c199ce19f85d105bd
Keshav <keshavraj936@gmail.com>
2025-09-28 19:07:44 +0530

    28th Sep

--- src/IdentityHashMapDemo.java
+++ src/IdentityHashMapDemo.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.IdentityHashMap;
 import java.util.Map;
 
 public class IdentityHashMapDemo {
@@ -5,6 +7,11 @@ public class IdentityHashMapDemo {
         String key1 = new String("key");
         String key2 = new String("key");
 
-        Map<String, Integer> map = new
+        Map<String, Integer> map = new IdentityHashMap<>();
+        map.put(key1, 1); //key, 1
+        map.put(key2, 2); //key, 2
+
+        System.out.println(key1.equals(key2));
+        System.out.println(map);
     }
 }
